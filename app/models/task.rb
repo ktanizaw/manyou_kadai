19e71d90e8870d8d9ab9b54fc8024e0cb4c334e8
tanizawa_kenta <kt10025102@gmail.com>
2019-12-11 20:16:58 +0900

    add:バリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,16 @@
 class Task < ApplicationRecord
+  validates :title,
+    presence: true, presence: {message: "入力してください！"},
+    length: { maximum: 15 }
+
+  validates :content,
+    length: { maximum: 100 }
+
+  validates :rank,
+    presence: true, presence: {message: "入力してください！"},
+    length: { maximum: 5 }
+
+  validates :status,
+    presence: true, presence: {message: "入力してください！"},
+    length: { maximum: 5 }
 end
