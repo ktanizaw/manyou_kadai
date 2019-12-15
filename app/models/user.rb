c759387da887b0c33f4855219eca09a62de81520
tanizawa_kenta <kt10025102@gmail.com>
2019-12-15 15:35:30 +0900

    add:emailのDB制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     presence: true,  presence: {message: "入力してください！"},
     uniqueness: true, length: { maximum: 10 }
 
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
     presence: true, presence: {message: "入力してください！"},
