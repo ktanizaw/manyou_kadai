74c3b94880e5ae1b66e09bd74dfcfc764d5d06cf
tanizawa_kenta <kt10025102@gmail.com>
2019-12-23 17:50:12 +0900

    add:labelテストコード

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -38,10 +38,14 @@ RSpec.describe 'ラベル管理機能', type: :system do
     describe 'タスク一覧・詳細画面' do
       context 'ラベル付タスクを作成した場合' do
         it 'タスク一覧にラベルが表示されること' do
+          visit new_session_path
+          fill_in 'Email', with: 'test1@test.com'
+          fill_in 'Password', with: 'test111'
+          click_on 'Log in'
           visit new_task_path
           fill_in "Title", with: "タイトル１"
           fill_in "Content", with: "コンテント１"
-          check "label_ids[1]"
+          check "test1"
           click_on 'Create Task'
           visit tasks_path
           expect(page).to have_content "test1"
@@ -52,9 +56,13 @@ RSpec.describe 'ラベル管理機能', type: :system do
     describe 'ラベル検索機能' do
       context 'ラベルで絞り込み検索した場合' do
         it '絞り込んだラベルが表示される' do
+          visit new_session_path
+          fill_in 'Email', with: 'test1@test.com'
+          fill_in 'Password', with: 'test111'
+          click_on 'Log in'
           visit tasks_path
-          select 'test1', from: 'label_ids[]'
-          click_on('絞り込み')
+          select 'test1'
+          find('#settings-link').click
           task_list = page.all('tr')
           expect(task_list[1]).to have_content 'test1'
           expect(task_list[2]).not_to have_content 'test2'
